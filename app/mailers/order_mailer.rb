cc4f08710a626ec69a0a6f9962e3f7253d7ff101
Matt Kelly <bmatthewkelly@gmail.com>
2018-08-16 23:56:20 +0000

    print order in email

--- app/mailers/order_mailer.rb
+++ app/mailers/order_mailer.rb
@@ -1,11 +1,10 @@
 class OrderMailer < ApplicationMailer
   default from: "no-reply@jungle.com"
-  # layout 'order_summary_email'
 
   def order_summary_email(order)
     @order = order
     @user = order.email
     
-    puts mail(to: @user, subject: 'Your Order Summary | Jungle')
+    puts mail(to: @user, subject: @order.id)
   end
 end
