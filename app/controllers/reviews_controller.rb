c99d96c1182da55f7b2d7d74c0cf41601437197b
Matt Kelly <bmatthewkelly@gmail.com>
2018-08-17 22:19:43 +0000

    complete step 12(delete button)

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,7 +3,6 @@ class ReviewsController < ApplicationController
     product = Product.find params[:product_id]
     current_user = User.find(session[:user_id]) if session[:user_id]
     @review = Review.new(review_params)
-
     @review.product_id = product.id
     @review.user_id = current_user.id
     if @review.valid?
@@ -12,14 +11,20 @@ class ReviewsController < ApplicationController
     redirect_to product
   end
 
+  def destroy
+    @review = Review.find(params[:id])
+    @review.destroy!
+    
+    redirect_to product_path(params[:product_id])
+  end
+
   private
 
   def review_params
     params[:product].require(:reviews).permit(
       :description,
       :rating,
-      :product_id,
-      :user_id
+      :product_id
     )
   end
 end
