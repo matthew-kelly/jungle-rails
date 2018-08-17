672bea0a6c8a3b572506474627863d62fce48612
Matt Kelly <bmatthewkelly@gmail.com>
2018-08-17 19:32:03 +0000

    complete steps 5 and 6: display reviews on product page

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,5 +4,5 @@ class Review < ActiveRecord::Base
   validates :product_id, presence: true
   validates :user_id, presence: true
   validates :description, presence: true, length: { minimum: 1 }
-  validates :rating, presence: true
+  validates :rating, presence: true, numericality: { only_integer: true, less_than: 6, greater_than: 0 }
 end
--- db/migrate/20180817174025_create_reviews.rb
+++ db/migrate/20180817174025_create_reviews.rb
@@ -1,12 +1,16 @@
 class CreateReviews < ActiveRecord::Migration
   def change
     create_table :reviews do |t|
-      t.integer :product_id
-      t.integer :user_id
+      # t.integer :product_id
+      # t.integer :user_id
+      t.references :product, foreign_key: true
+      t.references :user, foreign_key: true
       t.text :description
       t.integer :rating
 
       t.timestamps null: false
     end
+    add_index :reviews, :user_id
+    add_index :reviews, :product_id
   end
 end
